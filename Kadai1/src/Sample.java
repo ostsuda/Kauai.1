19eb4cf61eba89e0a4496b2c327ad63562202cf8
g16933os <g16933os@s30429.tsuda.ac.jp>
2018-04-12 14:28:49 +0900

    kadai1

---
+++ Kadai1/src/Sample.java
@@ -0,0 +1,10 @@
+
+public class Sample {
+ 
+	public static void main(String[] args) {
+		System.out.println("課題を何が何でも毎回出す");
+		System.out.println("きちんと意味を理解する");
+		System.out.println("こなすのではなく、主体性をもって行う");
+		System.out.println("初っ端からできない。。。");
+	}
+}
